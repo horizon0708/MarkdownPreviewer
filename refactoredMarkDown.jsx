e05f96c91e73c55b5872492f8bf31b7423090edf
Hyun Jong Kim <Aya@Hyuns-MacBook-Pro.local>
2017-07-03 13:55:32 +1200

    final version

---
+++ refactoredMarkDown.jsx
@@ -0,0 +1,37 @@
+var startText = 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n'
+
+class MainBody extends React.Component{
+    constructor (prop){
+        super(prop);
+        this.state = {rawtext: startText};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        this.setState({rawtext: e.target.value});
+    }
+    getMarkDownText(){
+        var text = marked(this.state.rawtext, {sanitize: true});
+        return {__html: text};
+    }
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6" id="input">
+                        <textarea type="text" onChange={this.handleChange}>
+                        {startText}
+                        </textarea>
+                    </div>
+                    <div className="col-md-6" id="output">
+                        <div dangerouslySetInnerHTML={this.getMarkDownText()} />                       
+                    </div>
+                </div>               
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <MainBody />,
+    document.getElementById('root')
+)
